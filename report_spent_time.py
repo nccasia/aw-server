1fb1f0da9a4297451d608c14eef6a8d9f1725bc7
Nhan Nguyen <nguyentran@ncc.asia>
2022-04-08 10:53:04 +0700

    insert to mongo

--- report_spent_time.py
+++ report_spent_time.py
@@ -1,6 +1,10 @@
 from datetime import datetime, time, timedelta
 import aw_client
+import pymongo
 
+myclient = pymongo.MongoClient("mongodb://localhost:27017/")
+mydb = myclient["komubot"]
+mycol = mydb["komu_tracker_spent_time"]
 
 def main():
     # You need to set testing=False if you're going to run this on your normal instance
@@ -8,7 +12,7 @@ def main():
 
     buckets = aw.get_buckets()
     #print(f"Buckets: {buckets.keys()}")
-
+    report_data = []
     for bucket_id in buckets.keys():
         try:
             daystart = datetime.combine(datetime.now().date(), time())
@@ -18,9 +22,12 @@ def main():
                 events = aw.get_events(bucket_id, start=daystart, end=dayend)
                 events = [e for e in events if e.data["status"] == "not-afk"]
                 total_duration = sum((e.duration for e in events), timedelta())
-                print(f"Total time spent on {bucket_id.split('_')[-1]} today: {total_duration}")
+                if total_duration.total_seconds() > 0:
+                    report_data.append({ "userid": bucket_id.split('_')[-1], "spent_time": total_duration.total_seconds(), "date" : datetime.now().strftime("%m/%d/%Y") })
         except Exception as e:
             print(f"Error: {e}")
+
+    mycol.insert_many(report_data)
 
 if __name__ == "__main__":
     main()
