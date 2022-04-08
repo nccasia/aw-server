07c2a0ee85395e09d0747a56058284ce95cf0952
NccSoft Vietnam <nguyentran@nccsoft.vn>
2022-04-08 20:31:31 +0700

    Update report_spent_time.py

--- report_spent_time.py
+++ report_spent_time.py
@@ -23,7 +23,7 @@ def main():
                 events = [e for e in events if e.data["status"] == "not-afk"]
                 total_duration = sum((e.duration for e in events), timedelta())
                 if total_duration.total_seconds() > 0:
-                    report_data.append({ "email: bucket_id.split('_')[-1], "spent_time": total_duration.total_seconds(), "date" : datetime.now().strftime("%m/%d/%Y") })
+                    report_data.append({ "email": bucket_id.split('_')[-1], "spent_time": total_duration.total_seconds(), "date" : datetime.now().strftime("%m/%d/%Y") })
         except Exception as e:
             print(f"Error: {e}")
 
