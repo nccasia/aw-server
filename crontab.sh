f7adf684b8b9d56ccd6bd43a2bacb1559149b222
Nhan Nguyen <nguyentran@ncc.asia>
2022-04-15 13:16:36 +0700

    remove unused file

--- crontab.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-# Used to create base .spec (needs manual modification)
-# pyinstaller __main__.py -n aw-server
-
-echo "Starting report..."
-
-/home/nccsoft/komutracker/venv/bin/python /home/nccsoft/komutracker/aw-server/report_spent_time.py
--- start.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-for i in `ps -aux | grep '__main__.py' | awk '{print $2}'`;
-    do kill -9 $i
-done
-
-cd /home/nccsoft/komutracker/aw-server
-nohup /home/nccsoft/komutracker/venv/bin/python __main__.py > aw-server-log.txt 2>&1 &
